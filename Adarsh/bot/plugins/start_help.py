4ba0119d149cd6f7701f1bfebc5a637482f4c2a3
Vishnu MB <137043572+wishvishnu179@users.noreply.github.com>
2024-10-31 18:00:59 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -13,6 +13,24 @@ from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
 db = Database(Var.DATABASE_URL, Var.name)
 from pyrogram.types import ReplyKeyboardMarkup
 
+@Client.on_message(filters.command("start") & filters.incoming)
+async def start(client, message):
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    rm = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton("✨ Update Channel", url="https://t.me/TGCINEMAWORLD")
+        ]] 
+    )
+    await client.send_message(
+        chat_id=message.from_user.id,
+        text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        reply_markup=rm,
+        parse_mode=enums.ParseMode.HTML
+    )
+    return
+  
                       
 @StreamBot.on_message(filters.command('start') & filters.private)
 async def start(b, m):
